fda09380add69e2748065bd79b06e71d3a2c2b34
Glitch (hello-express) <none>
2020-04-23 06:47:33 +0000

    🍨🐊 Checkpoint ./server.js:15261114/776

--- server.js
+++ server.js
@@ -114,7 +114,33 @@ app.post('/users/create' , (req, res) => {
     id : id
   }
   db.get('users').push(value).write();
-  res.redirerct('/user');
+  res.redirect('/users');
+})
+
+app.get('/users/search' , (req, res) => {
+  var search = req.query.search;
+  var list = db.get('users').value().filter(ele => {
+    return removeAccents(ele.name).toLowerCase().indexOf(removeAccents(search).toLowerCase()) !== -1 ;
+  })
+  res.render('user' , {
+    "users" : list
+  })
+})
+
+app.get('/users/:id' , (req, res) => {
+  var id = req.params.id;
+  db.get('users')
+    .remove({ id : id})
+    .write();
+  res.redirect('/users');
+})
+app.get('/users/edit/:id' , (req, res) => {
+  res.render()
+})
+
+app.post('/users/edit/:id' , (req, res) => {
+  var id = req.params.id;
+  db.ge
 })
 
 
