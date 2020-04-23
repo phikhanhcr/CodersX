590ead7b37e621878f4e2e83c7e96d811f0a7667
Glitch (hello-express) <none>
2020-04-23 07:30:45 +0000

    🚿🚆 Checkpoint ./server.js:15261114/4108 ./route.user.js:15261114/1754

--- route.user.js
+++ route.user.js
@@ -1,2 +1,73 @@
 const express = require('express');
 var router = express.Router();
+var db = require("./db.js");
+var pug = require("pug");
+var shortId = require("shortid");
+var removeAccents = require("./removeAccents.js");
+
+
+router.get("/users", (req, res) => {
+  var user = db.get("users").value();
+  res.render("user", {
+    users: user
+  });
+});
+router.get("/users/create", (req, res) => {
+  res.render("createUser");
+});
+router.post("/users/create", (req, res) => {
+  var name = req.body.name;
+  var sdt = req.body.sdt;
+  var id = shortId.generate();
+  var value = {
+    name: name,
+    sdt: sdt,
+    id: id
+  };
+  db.get("users")
+    .push(value)
+    .write();
+  res.redirect("/users");
+});
+
+router.get("/users/search", (req, res) => {
+  var search = req.query.search;
+  var list = db
+    .get("users")
+    .value()
+    .filter(ele => {
+      return (
+        removeAccents(ele.name)
+          .toLowerCase()
+          .indexOf(removeAccents(search).toLowerCase()) !== -1
+      );
+    });
+  res.render("user", {
+    users: list
+  });
+});
+
+router.get("/users/:id", (req, res) => {
+  var id = req.params.id;
+  db.get("users")
+    .remove({ id: id })
+    .write();
+  res.redirect("/users");
+});
+
+router.get("/users/edit/:id", (req, res) => {
+  res.render("userEdit");
+});
+
+router.post("/users/edit/:id", (req, res) => {
+  var id = req.params.id;
+  var value = req.body.edit;
+  db.get("users")
+    .find({ id: id })
+    .assign({ name: value })
+    .value();
+  res.redirect("/users");
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const express = require("express");
 const app = express();
 var pug = require("pug");
 var bodyParser = require("body-parser");
-var db = require("./db.js");
-var removeAccents = require("./removeAccents.js");
-var shortId = require("shortid");
+
 var bookRouter = require('./route.book.js');
+var userRouter = require('./route.user.js');
 // our default array of dreams
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -41,71 +40,9 @@ app.get("/dreams", (request, response) => {
   response.json(dreams);
 });
 
-app.use('/books' , bookRouter)
+app.use('/books' , bookRouter);
+app.use('/users' , userRouter);
 
-
-app.get("/users", (req, res) => {
-  var user = db.get("users").value();
-  res.render("user", {
-    users: user
-  });
-});
-app.get("/users/create", (req, res) => {
-  res.render("createUser");
-});
-app.post("/users/create", (req, res) => {
-  var name = req.body.name;
-  var sdt = req.body.sdt;
-  var id = shortId.generate();
-  var value = {
-    name: name,
-    sdt: sdt,
-    id: id
-  };
-  db.get("users")
-    .push(value)
-    .write();
-  res.redirect("/users");
-});
-
-app.get("/users/search", (req, res) => {
-  var search = req.query.search;
-  var list = db
-    .get("users")
-    .value()
-    .filter(ele => {
-      return (
-        removeAccents(ele.name)
-          .toLowerCase()
-          .indexOf(removeAccents(search).toLowerCase()) !== -1
-      );
-    });
-  res.render("user", {
-    users: list
-  });
-});
-
-app.get("/users/:id", (req, res) => {
-  var id = req.params.id;
-  db.get("users")
-    .remove({ id: id })
-    .write();
-  res.redirect("/users");
-});
-
-app.get("/users/edit/:id", (req, res) => {
-  res.render("userEdit");
-});
-
-app.post("/users/edit/:id", (req, res) => {
-  var id = req.params.id;
-  var value = req.body.edit;
-  db.get("users")
-    .find({ id: id })
-    .assign({ name: value })
-    .value();
-  res.redirect("/users");
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
