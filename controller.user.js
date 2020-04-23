c593b3dcbafa55dae0dab103b28b5ebba295b397
Glitch (hello-express) <none>
2020-04-23 14:57:57 +0000

    📯🏅 Checkpoint ./controller.user.js:15261114/3488 ./route.user.js:15261114/2781

---
+++ controller.user.js
@@ -0,0 +1,67 @@
+var db = require("./db.js");
+var pug = require("pug");
+var shortId = require("shortid");
+var removeAccents = require("./removeAccents.js");
+
+module.exports.index = (req, res) => {
+  var user = db.get("users").value();
+  res.render("user", {
+    users: user
+  });
+}
+
+module.exports.create = (req, res) => {
+  res.render("createUser");
+}
+module.exports.createPost = (req, res) => {
+  var name = req.body.name;
+  var sdt = req.body.sdt;
+  var id = shortId.generate();
+  var value = {
+    name: name,
+    sdt: sdt,
+    id: id
+  };
+  db.get("users")
+    .push(value)
+    .write();
+  res.redirect("/users");
+}
+
+module.exports.search =  (req, res) => {
+  var search = req.query.search;
+  var list = db
+    .get("users")
+    .value()
+    .filter(ele => {
+      return (
+        removeAccents(ele.name)
+          .toLowerCase()
+          .indexOf(removeAccents(search).toLowerCase()) !== -1
+      );
+    });
+  res.render("user", {
+    users: list
+  });
+}
+
+module.exports.remove =  (req, res) => {
+  var id = req.params.id;
+  db.get("users")
+    .remove({ id: id })
+    .write();
+  res.redirect("/users");
+}
+
+module.exports.edit = (req, res) => {
+  res.render("userEdit");
+}
+module.exports.editPost = (req, res) => {
+  var id = req.params.id;
+  var value = req.body.edit;
+  db.get("users")
+    .find({ id: id })
+    .assign({ name: value })
+    .value();
+  res.redirect("/users");
+}
--- route.user.js
+++ route.user.js
@@ -1,73 +1,20 @@
 const express = require('express');
 var router = express.Router();
-var db = require("./db.js");
-var pug = require("pug");
-var shortId = require("shortid");
-var removeAccents = require("./removeAccents.js");
+
+var controller = require('./controller.user.js');
 
 
-router.get("/users", (req, res) => {
-  var user = db.get("users").value();
-  res.render("user", {
-    users: user
-  });
-});
-router.get("/users/create", (req, res) => {
-  res.render("createUser");
-});
-router.post("/users/create", (req, res) => {
-  var name = req.body.name;
-  var sdt = req.body.sdt;
-  var id = shortId.generate();
-  var value = {
-    name: name,
-    sdt: sdt,
-    id: id
-  };
-  db.get("users")
-    .push(value)
-    .write();
-  res.redirect("/users");
-});
+router.get("/users", controller.index );
+router.get("/users/create", controller.create);
+router.post("/users/create", controller.createPost);
 
-router.get("/users/search", (req, res) => {
-  var search = req.query.search;
-  var list = db
-    .get("users")
-    .value()
-    .filter(ele => {
-      return (
-        removeAccents(ele.name)
-          .toLowerCase()
-          .indexOf(removeAccents(search).toLowerCase()) !== -1
-      );
-    });
-  res.render("user", {
-    users: list
-  });
-});
+router.get("/users/search", controller.search );
 
-router.get("/users/:id", (req, res) => {
-  var id = req.params.id;
-  db.get("users")
-    .remove({ id: id })
-    .write();
-  res.redirect("/users");
-});
+router.get("/users/:id", controller.remove );
 
-router.get("/users/edit/:id", (req, res) => {
-  res.render("userEdit");
-});
+router.get("/users/edit/:id", controller.edit);
 
-router.post("/users/edit/:id", (req, res) => {
-  var id = req.params.id;
-  var value = req.body.edit;
-  db.get("users")
-    .find({ id: id })
-    .assign({ name: value })
-    .value();
-  res.redirect("/users");
-});
+router.post("/users/edit/:id", controller.editPost );
 
 
 module.exports = router;
