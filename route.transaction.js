c5e2bd9aceaa1da63c32c901b75dae698b73ed75
Glitch (hello-express) <none>
2020-04-23 07:57:21 +0000

    👯🛋 Checkpoint ./db.json:15261114/9 ./db.js:15261114/22 ./route.transaction.js:15261114/1353

--- route.transaction.js
+++ route.transaction.js
@@ -6,8 +6,39 @@ var shortId = require("shortid");
 var removeAccents = require("./removeAccents.js");
 
 router.get('/' , (req, res) => {
-  var books = db.get('books').value();
-  res.render('list');
+  var transaction = db.get('transaction').value();
+  // 1 . find userId in transaction
+  var userId = transaction.map(ele => {    // []  array Id of db.get('transaction');
+    return ele.userId;
+  })
+  // 2 . get all object in user
+  var arrayUser = db.get('users').value();  // []
+  var userName = [];
+  // filter based on id
+  for(var i = 0 ; i < userId.length ; i++ ) {
+    arrayUser.filter(ele => {
+      if(ele.id = userId[i]) {
+        userName.push(userId[i])
+      }
+    })
+  }
+  
+  var booksId = transaction.map(ele => {    // []  array Id of db.get('transaction');
+    return ele.bookId;
+  })
+  var arrayBook = db.get('books').value();  // []
+  var bookName = [];
+  for(var i = 0 ; i < booksId.length ; i++ ) {
+    arrayBook.filter(ele => {
+      if(ele.id = booksId[i]) {
+        bookName.push(booksId[i])
+      }
+    })
+  }
+  res.render('transaction' , {
+    "books" : bookName, 
+    "users" : 
+  });
 })
 
 module.exports = router;
